3afd67d18722bc857317f585ce60ac8222cd82ca
anshulgusain <anshulgusain99@gmail.com>
2024-03-27 21:40:22 +0530

    navbar with all routes are made

---
+++ my-app/src/Components/Navbar.js
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom"
+
+import "../styles/navbar.css"
+
+const Navbar=()=>{
+    return(
+        <div>
+      <div className="parent">
+        <div className="container">
+            <Link to="/" className="heading">
+                <span>Taskify</span>
+            </Link>
+
+           <ul className="menu">
+            <li className="item">
+                <Link to= "/home" className="links">
+                    Home
+                </Link>
+            </li>
+            <li className="item">
+                <Link to= "/create" className="links">
+                    Create
+                </Link>
+            </li>
+            <li className="item">
+                <Link to= "/edit" className="links">
+                    Edit
+                </Link>
+            </li>
+            <li className="item">
+                <Link to= "/" className="links">
+                    About
+                </Link>
+            </li>
+            <li className="item">
+                <Link to= "/login" className="links">
+                    Login
+                </Link>
+            </li>
+           </ul>
+        </div>
+
+      </div>
+
+
+
+        </div>
+    )
+}
+export default Navbar
---
+++ my-app/src/Routes/AllRoutes.js
@@ -0,0 +1,25 @@
+import {Routes,Route} from "react-router-dom"
+
+import Home from "../Components/Home"
+import About from "../Components/About"
+import Create from "../Components/Create"
+import Edit from "../Components/Edit"
+import Login from "../Components/Login"
+import Signup from "../Components/Signup"
+import Navbar from "../Components/Navbar"
+
+const AllRoutes=()=>{
+    return<>
+    <Navbar />
+    <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/create" element={<Create />}></Route>
+        <Route path="/edit" element={<Edit />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+    </Routes>
+    </>
+    }
+    
+    export default AllRoutes
